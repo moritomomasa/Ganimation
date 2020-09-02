b6115bae6df0222ec0f1d94bfb2bb1664ac9ae8d
yutatonoda <68141589+yutatonoda@users.noreply.github.com>
2020-09-02 15:29:39 +0900

    Add files via upload

---
+++ rec1.py
@@ -0,0 +1,28 @@
+import cv2
+import os
+
+def extractFrames(pathIn, pathOut):
+    if not os.path.exists(pathOut):
+        os.mkdir(pathOut)
+
+    cap = cv2.VideoCapture(pathIn)
+    count = 0
+
+    while (cap.isOpened()):
+
+        ret, frame = cap.read()
+
+        if ret == True:
+            cv2.imwrite(os.path.join(pathOut, "frame_{:06d}.jpg".format(count)), frame)
+            count += 1
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def main():
+    extractFrames('video.mp4', 'outputdir')
+
+if __name__=="__main__":
+    main()
